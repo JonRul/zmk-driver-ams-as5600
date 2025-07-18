8b2d1ae1a2e9fd1322962fbe299a0d9614958d4e
Jonas Rulik <jonas.rulik@buchinger-wilhelmi.com>
2025-07-18 12:21:24 +0200

    hardcoded scale factor 100

--- src/zmk_input_ams_as5600.c
+++ src/zmk_input_ams_as5600.c
@@ -116,7 +116,7 @@ static int zmk_input_ams_as5600_process(const struct device *dev) {
     /* Only report input when rotation was detected */
     if (pulses) {
         // hardcoded temp
-        err = input_report_rel(dev, INPUT_REL_WHEEL, pulses / 30, true, K_FOREVER);
+        err = input_report_rel(dev, INPUT_REL_WHEEL, pulses / 100, true, K_FOREVER);
         if (err) {
             LOG_ERR(ZMK_INPUT_AMS_AS5600_LOG_PREFIX "Failed to report input value: %d", err);
             return err;
